571dc3a882ea9d67976e898ea4cf70eb7406e47a
Bruno Tetzner <brunoltetzner@gmail.com>
2022-07-06 19:21:04 -0300

    feat: rota de post e get de tasks criadas

--- app/controllers/tasks_controller.py
+++ app/controllers/tasks_controller.py
@@ -1,8 +1,21 @@
+import json
+from flask import jsonify, request
+from app.models import TaskModel
+from flask import current_app
+
 def create_task_controller():
-    return "creating", 201
+    data = request.get_json()
+    session = current_app.db.session
+    new_data = TaskModel(**data)
+    session.add(new_data)
+    session.commit()
+
+    return jsonify(new_data), 201
+   
 
 def get_task_controller():
-    return "getting", 200
+   all_tasks = TaskModel.query.all()
+   return jsonify(all_tasks)
 
 def patch_task_controller():
     return "patching", 200
